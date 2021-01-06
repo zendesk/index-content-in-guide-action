74006cee0593b08a102c5cb9f4c4b965ecb76c09
Daniel Schierbeck <daniel@dasch.io>
2021-01-06 14:02:56 +0100

    Format log messages with colors

--- sync.rb
+++ sync.rb
@@ -153,6 +153,7 @@ def main
   Dir.chdir WORKING_DIR
 
   logger = Logger.new(STDOUT)
+  logger.formatter = ColoredLoggingFormatter
 
   contents = Content.load_all(CONTENT_DIR)
   api = FederatedSearchAPI.new(logger: logger)
@@ -206,6 +207,41 @@ def main
     end
 
     logger.info "DONE!"
+  end
+end
+
+module ColoredLoggingFormatter
+  COLORS = {
+    black: 30,
+    blue:  34,
+    yellow: 33,
+    cyan: 36,
+    green: 32,
+    magenta: 35,
+    red: 31,
+    white: 37
+  }
+
+  class << self
+    def call(severity, datetime, progname, msg)
+      color = case severity
+      when "DEBUG" then :cyan
+      when "INFO" then :white
+      when "WARN" then :yellow
+      when "ERROR" then :yellow
+      end
+
+      code = COLORS.fetch(color)
+
+      [
+        "\e[#{code}m",
+        severity.ljust(5, " "),
+        "[" + datetime.strftime("%k:%M") + "]",
+        msg,
+        "\033[0m",
+        "\n",
+      ].join("  ")
+    end
   end
 end
 
